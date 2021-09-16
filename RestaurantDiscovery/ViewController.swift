f9625739593f32ef14128a94385f36928155a872
nick <nickshi@users.noreply.github.com>
2021-09-16 15:20:19 -0500

    Initial Commit

---
+++ RestaurantDiscovery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantDiscovery
+//
+//  Created by Nick Shi on 9/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
